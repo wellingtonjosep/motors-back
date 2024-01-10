000639992c35a02be5133fe7109a6d16c27f189c
Wellington Paiva <paivawellington23@gmail.com>
2024-01-10 20:42:21 -0300

    feat: created project

---
+++ src/main/java/com/example/motors/MotorsApplication.java
@@ -0,0 +1,13 @@
+package com.example.motors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/motors/MotorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.motors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
